3b28b7e56d35cb83fd6b406a9df81d42fd7abc81
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-02 16:48:00 -0700

    Initial Commit

---
+++ AnimatingGestures/AnimatingGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingGesturesApp.swift
+//  AnimatingGestures
+//
+//  Created by RachelleLee on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
